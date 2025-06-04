7db82d5220ce9b6760a08e1cd3dd1d549d33701a
Mitchell Tracy <mitchell@aztecprotocol.com>
2025-06-04 08:37:43 -0400

    pulling lines

--- hello.py
+++ hello.py
@@ -1,5 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def main():
-    print("Hello from worstflakes!")
+    # use requests to go get "http://ci.aztec-labs.com/?filter=^next&filter_prop=name&fail_list=failed_tests_next"
+    response = requests.get(
+        "http://ci.aztec-labs.com/?filter=^next&filter_prop=name&fail_list=failed_tests_next"
+    )
+
+    # extract the body of the response
+    body = response.text
+    # parse the body as html
+    soup = BeautifulSoup(body, "html.parser")
+    # treat the body as a list of lines
+    lines = body.split("\n")
+    # find the lines that end in (target: next)
+    for line in lines:
+        if line.endswith("(target: next)"):
+            print(line)
 
 
 if __name__ == "__main__":
